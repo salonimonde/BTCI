7e4cc53e348ba7545525e368713dcf341fc309e3
Saloni Monde <saloni.monde@bynry.com>
2018-09-21 18:49:29 +0530

    Login Activity and Create Profile Activity

--- app/src/main/java/com/example/admin/bpalapplication/CreateProfileActivity.java
+++ app/src/main/java/com/example/admin/bpalapplication/CreateProfileActivity.java
@@ -3,11 +3,11 @@ package com.example.admin.bpalapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CreateProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_create_profile);
     }
 }
---
+++ app/src/main/java/com/example/admin/bpalapplication/LoginActivity.java
@@ -0,0 +1,45 @@
+package com.example.admin.bpalapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private RelativeLayout relativeLayoutLoginScreen;
+    private RelativeLayout relativeLayoutCongratulationsScreen;
+    private Button btnLetsCreateBpalProfile;
+    private Button btnLoginWithGoogle;
+    private Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mContext = this;
+        relativeLayoutLoginScreen = findViewById(R.id.relative_layout_login_screen);
+        relativeLayoutCongratulationsScreen = findViewById(R.id.relative_layout_congratulaions_screen);
+        btnLoginWithGoogle = findViewById(R.id.btn_login_with_google);
+        btnLoginWithGoogle.setOnClickListener(this);
+        btnLetsCreateBpalProfile = findViewById(R.id.btn_lets_create_bpal_profile);
+        btnLetsCreateBpalProfile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view == btnLoginWithGoogle){
+            relativeLayoutCongratulationsScreen.setVisibility(View.VISIBLE);
+        }
+        if (view == btnLetsCreateBpalProfile){
+            Intent intent = new Intent(mContext,CreateProfileActivity.class);
+            startActivity(intent);
+        }
+
+    }
+}
